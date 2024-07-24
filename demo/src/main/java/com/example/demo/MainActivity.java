df8b1d45bcf90cb6a0ee791980e6a902faad6c5e
duy <duy@mail.com>
2024-07-24 23:43:17 +0700

    Update to Android 34, latest build tools

--- demo/src/main/java/com/example/demo/MainActivity.java
+++ demo/src/main/java/com/example/demo/MainActivity.java
@@ -1,19 +1,19 @@
 package com.example.demo;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import org.apache.commons.io.FileUtils;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
 
 import VideoHandle.EpEditor;
 import VideoHandle.EpVideo;
-import VideoHandle.OnEditorListener;
+import ffmpeg.OnFfmpegProcessCallback;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -34,21 +34,22 @@ public class MainActivity extends AppCompatActivity {
             epVideo.rotation(90,true);
 
             EpEditor.OutputOption outputOption = new EpEditor.OutputOption(outputFile.getAbsolutePath());
-            EpEditor.exec(epVideo,
-                    outputOption, new OnEditorListener() {
+            EpEditor.INSTANCE.exec(epVideo,
+                    outputOption, new OnFfmpegProcessCallback() {
+                        @Override
+                        public void onProgress(long duration) {
+                            Log.d(TAG, "onProgress() called with: progress = [" + duration + "]");
+                        }
+
+                        @Override
+                        public void onFailure(@Nullable Exception error) {
+                            Log.d(TAG, "onFailure() called");
+
+                        }
+
                         @Override
                         public void onSuccess() {
                             Log.d(TAG, "onSuccess() called");
-                        }
-
-                        @Override
-                        public void onFailure() {
-                            Log.d(TAG, "onFailure() called");
-                        }
-
-                        @Override
-                        public void onProgress(float progress) {
-                            Log.d(TAG, "onProgress() called with: progress = [" + progress + "]");
                         }
                     });
         } catch (IOException e) {
